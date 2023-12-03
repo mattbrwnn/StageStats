0b27139d51cde4c6e03c10bcfabd7881bb85efd2
David A <alvarez.d@ufl.edu>
2023-12-03 14:36:22 -0500

    api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# todo/todo_api/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class SetListAPI(APIView):
+    # add permission to check if user is authenticated
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        
+        return Response("Response data song list")
